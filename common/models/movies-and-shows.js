da7acd2c2a5d497694e9350ef82e740baaf46f0a
Tyler Proctor <tproctor4414@hotmail.com>
2018-10-17 15:11:58 -0700

    Added comments and updated README.

--- common/models/movies-and-shows.js
+++ common/models/movies-and-shows.js
@@ -11,6 +11,7 @@ module.exports = function(Moviesandshows) {
 		returns: {arg: 'name', type: 'object'}
 	});
 
+	//Initialize custom endpoint to retrieve a movie by its ID
 	Moviesandshows.findMovieById = function(movieId, callback){
 		Moviesandshows.findOne({where: {id: movieId, type: 'movie'}}, function(err, data){
 			callback(null, data);			 
@@ -27,6 +28,7 @@ module.exports = function(Moviesandshows) {
 		returns: {arg: 'name', type: 'object'}
 	});
 
+	//Initialize custom endpoint to retrieve a show by its ID
 	Moviesandshows.findShowById = function(showId, callback){
 		Moviesandshows.findOne({where: {id: showId, type: 'show'}}, function(err, data){
 			callback(null, data)
@@ -43,9 +45,46 @@ module.exports = function(Moviesandshows) {
 		returns: {arg: 'name', type: 'object'}
 	});
 
+	//Initialize custom endpoint to retrieve a movie by its title
 	Moviesandshows.searchMovie = function(title, callback){
 		Moviesandshows.find({where: {name: title}}, function(err, data){
 			callback(null, data)
+		})
+	};
+
+	//========================================================
+	//				Display a random movie
+	//========================================================
+	Moviesandshows.remoteMethod("random", {
+		http: {path: '/random', verb: 'get'},
+		isStatic: true,
+		returns: {arg: 'name', type: 'object'}
+	});
+
+	//Initialize custom endpoint to display a random movie
+	Moviesandshows.random = function(callback){
+		Moviesandshows.find(function(err, data){
+			let n = data.length;
+			let rand = Math.floor(Math.random() * n);
+			callback(null, data[rand])
+		});
+	};
+
+
+	//========================================================
+	//				Find movie by Genre
+	//========================================================
+	Moviesandshows.remoteMethod("genre", {
+		accepts: {arg: 'genre', type: 'string', required: true},
+		isStatic: true,
+		http: {path: '/genre/:genre', verb: 'get'},
+		returns: {arg: 'name', type: 'object'}
+	});
+
+	//Initialize custom endpoint to retrieve a list of movies by their genre
+	Moviesandshows.genre = function(genre, callback){
+		Moviesandshows.find({where: {genre1: genre, type: 'movie'}}, function(err, data){
+			callback(null, data);			 
 		})
 	};
 };
